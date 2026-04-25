5395f6361ec2b991a3ed525bb56cd196f21be187
yowbibs <202380024@psu.palawan.edu.ph>
2026-04-25 19:31:17 +0800

    fix again

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # 👇 ADD THIS PART
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin, 'admin@gmail.com ', 'admin123')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com ', 'admin123')" | python manage.py shell
