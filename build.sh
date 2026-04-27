f20ad677eb82f159ce7dc0c42052c774185be66b
yowbibs <202380024@psu.palawan.edu.ph>
2026-04-28 00:19:16 +0800

    Add migrations to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-# 👇 ADD THIS PART
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@gmail.com ', 'admin123')" | python manage.py shell
